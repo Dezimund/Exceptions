e5e1045e24fb07d201c77afbaa72d9454c1fae0e
Dezimund <de3mond15@gmail.com>
2022-05-04 14:11:18 +0300

    Initial commit

---
+++ src/Task1/Main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        
+
+    }
+}
